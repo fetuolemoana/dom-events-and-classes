2891a499c4b3f2fd3d0b5f628d2dc7d99ee78123
fetuolemoana <fetu.tamapeau@gmail.com>
2018-07-28 13:45:02 +1200

    added green counter

--- game.js
+++ game.js
@@ -50,8 +50,11 @@ var allTheDots = document.getElementsByClassName('board')[0].children
     for (var i = 0; i < allTheDots.length; i++) {
       if (allTheDots[i].classList.contains('blue')) {
     //+ 1 to the totals.blue property 
-    totals.blue += 1 
-  }
+    totals.blue += 1; 
+      }
+    else if (allTheDots[i].classList.contains('green')) {
+      totals.green += 1; 
+    }
 }
 
   // Once you've done the counting, this function will update the display
